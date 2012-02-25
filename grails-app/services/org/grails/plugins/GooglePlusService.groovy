b5154343831569467fc0b45df7bb2d4edc7319d8
Vishal-Sahu <vishal@intelligrape.com>
2012-02-25 18:32:24 +0530

    Working on API call methods for Google PLus

--- grails-app/services/org/grails/plugins/GooglePlusService.groovy
+++ grails-app/services/org/grails/plugins/GooglePlusService.groovy
@@ -9,7 +9,6 @@ import org.grails.plugins.googlePlus.Person.Person
 class GooglePlusService {
     static transactional = false
 
-//    private static final googleProfileUrl = "https://www.googleapis.com/oauth2/v1/userinfo"
     private static final googleProfileUrl = "https://www.googleapis.com/oauth2/v1/userinfo"
     private static final String BASE_URL = "https://www.googleapis.com/plus/"
     public String accessToken
@@ -27,7 +26,6 @@ class GooglePlusService {
         Person person
         try {
             URL url = new URL("https://www.googleapis.com/plus/v1/people/me?access_token=${this.accessToken}")
-//            URL url = new URL(googleProfileUrl + "?access_token=${accessToken}")
             def jsonString = JSON.parse(url?.text)
             println "URL : " + url.toString()
             println "JSON : " + jsonString
