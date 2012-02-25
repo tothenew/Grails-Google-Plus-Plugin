d28a72625a888a8c7daca00316544fee53b8a890
Manoj <manoj@intelligrape.com>
2012-02-25 15:02:07 +0530

    Added Taglib to render Google Plus Button Access Token retrieved from Service

---
+++ grails-app/services/org/grails/plugins/AccessTokenService.groovy
@@ -0,0 +1,44 @@
+package org.grails.plugins
+
+import grails.converters.JSON
+
+class AccessTokenService {
+
+    static transactional = true
+
+    String generateAccessToken(String code) {
+        StringBuilder sb = new StringBuilder("code=");
+        sb.append(URLEncoder.encode(code, "UTF-8"));
+        sb.append("&client_id=");
+        sb.append(URLEncoder.encode("188806017778-omt5riisuev1ogqghupqgojmc7jdu25c.apps.googleusercontent.com", "UTF-8"));
+        sb.append("&client_secret=");
+        sb.append(URLEncoder.encode("dZb8-ccIq3WitqVhd1nSc5Up", "UTF-8"));
+        sb.append("&redirect_uri=");
+        sb.append(URLEncoder.encode("http://localhost:8080/SampleApp/util", "UTF-8"));
+        sb.append("&grant_type=");
+        sb.append(URLEncoder.encode('authorization_code', "UTF-8"));
+
+        String URL_TO_REQUEST_TOKEN = 'https://accounts.google.com/o/oauth2/token'
+
+        URL url = new URL(URL_TO_REQUEST_TOKEN);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try {
+            connection.setRequestMethod("POST");
+            connection.doOutput = true
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + sb.toString().length());
+            connection.setRequestProperty("Host", "accounts.google.com");
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+            outputStreamWriter.write(sb.toString());
+            outputStreamWriter.flush();
+            log.debug("Response code ${connection.responseCode} , Message : ${connection.responseMessage}")
+            String resultData = connection.content.text
+            def responseJson = JSON.parse(resultData)
+            return responseJson?.access_token
+        }
+        catch (Exception e) {
+            e.printStackTrace()
+        }
+
+    }
+}
---
+++ grails-app/taglib/org/grails/plugins/AccessTagLib.groovy
@@ -0,0 +1,15 @@
+package org.grails.plugins
+class AccessTagLib {
+
+    static namespace = "gp"
+
+    def registerOnGooglePlus = {attrs ->
+        String scope = attrs['googlePlusScope'] ?: "https://www.googleapis.com/auth/plus.me"
+        String callbackUrl = attrs['callbackUrl']
+        String clientId = attrs['clientId']
+        String buttonName = attrs['buttonName'] ?: 'Connect to Google+'
+        String authorizeUrl = "https://accounts.google.com/o/oauth2/auth?scope=${scope}&redirect_uri=${callbackUrl}&response_type=code&client_id=${clientId}&access_type=offline"
+        out << "<input type='button' value=${buttonName} onclick=\"location.href='${authorizeUrl}'\">"
+    }
+
+}
