5aeb4a15c12204bf5f9ca36e48ed4bd473fea638
Vishal-Sahu <vishal@intelligrape.com>
2012-04-13 20:35:57 +0530

    Working on Google plus plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,6 +13,7 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
+        grailsRepo "http://grails.org/plugins"
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
